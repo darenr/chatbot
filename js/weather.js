421f2d707470d363d88396ab9776ba0b62a6b847
drace <daren.race@gmail.com>
2017-07-07 23:38:33 -0700

    use yahoo weather

--- js/weather.js
+++ js/weather.js
@@ -5,14 +5,31 @@ function weatherEngine() {
       return "weatherEngine";
     },
     react: function(query) {
-      var zipcodePattern = /weather\s*(in)?\s*(\d{5})/i;
-      if (query.match(zipcodePattern)) {
-        var zipcode = query.match(zipcodePattern)[2];
+      var q = query.replace('?', '')
+      var zipcodePattern = /weather\s+(in\s+)?(\d{5})/i;
+      var cityPattern = /weather\s+(in)\s+(.*$)/i;
+      if (q.match(zipcodePattern)) {
+        var zipcode = q.match(zipcodePattern)[2];
         console.log("found weather intent with zip code of " + zipcode);
-        // do zipcode lookup
-        var content = "found weather intent with zip code of " + zipcode;
+        // docs: https://developer.yahoo.com/weather/
+        var select = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + zipcode + '")';
+        var url = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(select) + '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'jsonp'
+          })
+          .done(function(data) {
+            console.log(data);
+            ChatBot.addChatEntry(data.query.results.channel.title, 'bot');
+          });
+      } else if (q.match(cityPattern)) {
+        var city = q.match(cityPattern)[2];
+        console.log("found weather intent with zip code of " + city);
+        // do city lookup
+        var content = "found weather intent with city of " + city;
         ChatBot.addChatEntry(content, 'bot');
-      } else if (query.match(/.*(weather here|my weather).*$/i)) {
+      } else if (q.match(/.*(weather here|my weather).*$/i)) {
         // use html5 lat/lon
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
@@ -40,6 +57,7 @@ function weatherEngine() {
     getCapabilities: function() {
       return [
         "Ask about the weather like 'What is my weather?'",
+        "Ask about the weather like 'What is weather in San Francisco?'",
         "Ask about the weather in a certain zipcode, like 'What is the weather in 94065?'"
       ];
     },
